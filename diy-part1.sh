2bc42f2dbe974e0292d76fd014774c74fa2ccb71
dzhuqi <dzhuqi567@gmail.com>
2024-03-02 12:54:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # 查看所有标签
 #git tag
 # 切换到标签v22.03.6
-git checkout v22.03.6
+#git checkout v22.03.6
 
 # 回退源码
 #git reset --hard f372b71 #等同于切换到标签v22.03.6
@@ -24,16 +24,16 @@ git checkout v22.03.6
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 注释默认 packages
-sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
+#sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
 # 添加 packages
-sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git;22.03' feeds.conf.default
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 
 # 注释默认 luci
-sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
+#sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
 # 添加 luci
 #sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;22.03' feeds.conf.default
-sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git^db0ddd1' feeds.conf.default
+#sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git^db0ddd1' feeds.conf.default
 
 # 添加 lienol 大的 package
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
-sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
+#sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
